ef5ffc731efd740e6590bb28ac368c0906c6c593
Nicolas <mbongnicolas@gmail.com>
2023-01-25 01:05:24 +0100

    testScores style's updated

--- PL/Views/testScores.php
+++ PL/Views/testScores.php
@@ -52,13 +52,42 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Styles/style_testScores.css">
     <title>Scores</title>
 </head>
 <body>
-    <?php 
-        echo "number of correct scores: " . $correct;
-    }
-   ?>
+   <header>
+        <div class="container">
+            <div class="logo">
+                <img src="../Assets/image/flags.jpg" alt="" style="width: 100px;">
+            </div>
+
+            <div class="onglet">
+                <ul>
+                    <li><a href="../Views/home.php">Accueil</a></li>
+                    <li><a href="../Views/verbs.php">Liste des verbes</a></li>
+                    <li><a href="../Views/pretest.php">English Test</a></li>
+                    <li><a href="../Views/account.php">Mon compte</a></li>
+                    <?php 
+                        if (!isset($_SESSION['loggedUser'])) {
+                            echo '<li><a href="../Views/loginForm.php">Login</a></li>';
+                        }else{
+                            echo '<li><a href="../Views/logout.php">Logout</a></li>';
+                        }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div class="boite">
+        <div  class="box">
+            <p>
+                <?php 
+                echo "number of correct scores: " . $correct;
+            }
+        ?></p>
+    </div>
+   </div>
 </body>
 </html>
 
